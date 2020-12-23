60da0a925980495810470dac89f73ce8bc4ae3e7
Yazid Mekhtoub <ymekhtoub@gmail.com>
2020-12-22 23:15:54 -0500

    add the title

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,17 +1,37 @@
+import json
+import urllib.request
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
 from flask_login import login_required
 from flask_paginate import Pagination, get_page_args
 
-import requests
-url = "https://raw.githubusercontent.com/amazighy/files/main/lectures.txt"
-data = requests.get(url).content
-data = data.decode("utf-8")
-lectu = data.split("\n")
-lectu.pop()
-
-
 main = Blueprint('main', __name__)
+
+with urllib.request.urlopen("https://raw.githubusercontent.com/amazighy/files/main/lectures.json") as url:
+    data = json.loads(url.read().decode())
+title = list(data.keys())
+values = list(data.values())
+
+
+def get_users(offset=0, per_page=1):
+    return values[offset: offset + per_page]
+
+
+@main.route("/lectures")
+@login_required
+def lectures():
+    page, _, _ = get_page_args(page_parameter='page',
+                               per_page_parameter='per_page')
+    total = len(values)
+    pagination = Pagination(page=page, per_page=1, total=total,
+                            css_framework='bootstrap4')
+    return render_template('lectures.html',
+                           values=values,
+                           page=page,
+                           title=title,
+                           pagination=pagination,
+
+                           )
 
 
 @main.route("/home")
@@ -26,27 +46,3 @@ def home():
 @main.route("/")
 def syllabus():
     return render_template('syllabus.html', title='Syllabus')
-
-
-users = lectu
-
-
-def get_users(offset=0, per_page=1):
-    return users[offset: offset + per_page]
-
-
-@main.route("/lectures")
-@login_required
-def lectures():
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    total = len(users)
-    pagination_users = get_users(offset=1, per_page=1)
-    pagination = Pagination(page=page, per_page=1, total=total,
-                            css_framework='bootstrap4')
-    return render_template('lectures.html',
-                           users=users,
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination,
-                           )
